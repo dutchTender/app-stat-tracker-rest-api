45a25da004ca4b24c9f09403830cea7776d9f23c
lzhang <lzhang421@gmail.com>
2026-09-17 22:07:29 -0400

    integrated auth endpoint for bearer token generation and processing.
    all endpoints are now jwt protected.

---
+++ app-stat-tracker-rest-api/src/main/java/li/zhang/app_stat_tracker_rest_api/controller/AuthController.java
@@ -0,0 +1,44 @@
+package li.zhang.app_stat_tracker_rest_api.controller;
+
+
+
+
+
+
+import li.zhang.app_stat_tracker_rest_api.services.TokenService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    private final TokenService tokenService;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
+    public AuthController(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
+
+    @PostMapping("/auth")
+    public ResponseEntity<String> token(Authentication authentication) {
+        // this will be updated to a authorization token exchange
+        var token = this.tokenService.generateAccessToken(authentication);
+        logger.info("Generated token: {}", token);
+        return ResponseEntity.ok(token);
+
+    }
+
+
+
+}
+
+
