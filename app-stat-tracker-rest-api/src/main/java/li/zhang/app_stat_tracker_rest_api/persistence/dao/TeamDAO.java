768829d77fbc84dfc06b1d425eb1a9978ae406b5
lzhang <lzhang421@gmail.com>
2026-09-20 11:37:14 -0400

    created DAO for TEAM.  need cucmber test next.

--- app-stat-tracker-rest-api/src/main/java/li/zhang/app_stat_tracker_rest_api/persistence/dao/TeamDAO.java
+++ app-stat-tracker-rest-api/src/main/java/li/zhang/app_stat_tracker_rest_api/persistence/dao/TeamDAO.java
@@ -1,4 +1,13 @@
 package li.zhang.app_stat_tracker_rest_api.persistence.dao;
 
-public class TeamDAO {
+import li.zhang.app_stat_tracker_rest_api.persistence.entity.Team;
+import org.springframework.data.domain.Example;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.QueryByExampleExecutor;
+import java.util.Optional;
+
+public interface TeamDAO extends JpaRepository<Team, Long>, QueryByExampleExecutor<Team> {
+    Optional<Team> findTeamById(Long id);
+    Optional<Team> findTeamByTeamName(String teamName);
+    Optional<Team> findTeamBy(Example<Team> example);
 }
