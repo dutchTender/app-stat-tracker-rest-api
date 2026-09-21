0e19b348d986e26ade86586beacf2eebcba51143
lzhang <lzhang421@gmail.com>
2026-09-20 22:10:54 -0400

    updated seeder. also verified child object mappers works.

--- app-stat-tracker-rest-api/src/main/java/li/zhang/app_stat_tracker_rest_api/services/util/SeedService.java
+++ app-stat-tracker-rest-api/src/main/java/li/zhang/app_stat_tracker_rest_api/services/util/SeedService.java
@@ -1,15 +1,24 @@
 package li.zhang.app_stat_tracker_rest_api.services.util;
 
 import li.zhang.app_stat_tracker_rest_api.persistence.dao.PlayerDAO;
+import li.zhang.app_stat_tracker_rest_api.persistence.dao.TeamDAO;
 import li.zhang.app_stat_tracker_rest_api.persistence.entity.Player;
+import li.zhang.app_stat_tracker_rest_api.persistence.entity.Team;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class SeedService {
 
     private final PlayerDAO playerDAO;
-    public SeedService(PlayerDAO playerDAO) {
+    private final TeamDAO teamDAO;
+    public SeedService(PlayerDAO playerDAO,  TeamDAO teamDAO) {
+
         this.playerDAO = playerDAO;
+        this.teamDAO = teamDAO;
     }
 
 
@@ -25,7 +34,7 @@ public class SeedService {
         this.playerDAO.saveAndFlush(player1);
 
         Player player2 = new Player();
-        player2.setUserName("black]lighting");
+        player2.setUserName("black-lighting");
         player2.setEmail("lzhang4333@gmailo.com");
         player2.setFirstName("mike");
         player2.setLastName("johnson");
@@ -35,7 +44,7 @@ public class SeedService {
         this.playerDAO.saveAndFlush(player2);
 
         Player player3 = new Player();
-        player3.setUserName("whilethunder");
+        player3.setUserName("white-thunder");
         player3.setEmail("xxxx@gmailo.com");
         player3.setFirstName("nick");
         player3.setLastName("price");
@@ -43,5 +52,22 @@ public class SeedService {
         player3.setPhone("222-555-7777");
 
         this.playerDAO.saveAndFlush(player3);
+
+        Set<Player> players = new HashSet<>();
+        players.add(player1);
+        players.add(player2);
+        players.add(player3);
+
+        Team team1 = new Team("Team-Thunder");
+        team1.setTeamCoachName("Dutch Tender");
+        team1.setPlayers(players);
+        this.teamDAO.saveAndFlush(team1);
+        player1.setTeam(team1);
+        player2.setTeam(team1);
+        player3.setTeam(team1);
+        this.playerDAO.saveAndFlush(player1);
+        this.playerDAO.saveAndFlush(player2);
+        this.playerDAO.saveAndFlush(player3);
+
     }
 }
